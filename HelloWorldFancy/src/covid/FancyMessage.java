67ee2c5856f1d8980428e3d794bd793e1a8d53e7
James M <jamie.mih@gmail.com>
2020-03-24 15:38:25 -0400

    Added main

---
+++ HelloWorldFancy/src/covid/FancyMessage.java
@@ -0,0 +1,13 @@
+package covid;
+
+public class FancyMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer s = new StringBuffer("How about that!!!");
+		System.out.println(s.toString());
+
+	}
+
+}
